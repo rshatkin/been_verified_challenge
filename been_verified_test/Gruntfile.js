b20048e857db6fe4ff42ad8e59be2660861b0f01
rshatkin <shatkin.r@gmail.com>
2016-12-06 13:38:15 -0500

    all the things

---
+++ been_verified_test/Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'stylesheets/app.css' : 'sass/app.scss'
+					// 'stylesheets/homepage.css' : 'sass/homepage.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
---
+++ been_verified_test/scripts/test.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".navbar-toggle").on("click", function () {
+	    $(this).toggleClass("active");
+  });
+});
+
